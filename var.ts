23e39cb6539c4f721fde1d26a9ba6f56f2dded71
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-09-11 21:13:02 +0530

    var

---
+++ var.ts
@@ -0,0 +1,16 @@
+function funs(){
+    var sno:number=101;
+    let x:number=1;
+    alert("Hello, world!"+sno+x);
+
+}
+funs();
+
+function fun1(){
+    {
+        var sno:number=102;
+        var x=3;
+    }
+    alert("Hello, world!"+sno+x);
+}
+fun1();
