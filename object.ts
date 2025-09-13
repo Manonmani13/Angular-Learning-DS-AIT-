84c798366f4f61a8a77161e83784b7c5ad63fed8
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-09-13 21:15:04 +0530

    sample obj

---
+++ object.ts
@@ -0,0 +1,14 @@
+var user:any={
+    uname:"scott",
+    wife:"sw",
+    child:{son:["aa","bb"],daughter:"cc"}
+}
+
+for(var key in user){
+    console.log(key + " : " + user[key]);
+    alert(key);
+    for(var k in user[key]){
+        console.log(k + " : " + user[key][k]);
+        alert(k);
+    }
+}
