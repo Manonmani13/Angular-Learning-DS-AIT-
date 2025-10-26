860f011dd64924029f30ee2eb3cb75fb9198d223
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-10-26 19:10:48 +0530

    first

---
+++ firstClassEx.js
@@ -0,0 +1,8 @@
+var cls1 = /** @class */ (function () {
+    function cls1() {
+        this.sno = 100;
+    }
+    return cls1;
+}());
+var obj1 = new cls1(); //way of creating object
+alert(obj1.sno);
---
+++ firstClassEx.ts
@@ -0,0 +1,5 @@
+class cls1{
+    sno: number=100;
+}
+var obj1:cls1=new cls1();//way of creating object
+alert(obj1.sno);
